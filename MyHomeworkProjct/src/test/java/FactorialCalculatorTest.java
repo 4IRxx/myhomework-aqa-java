58d0eab1d21eeadfca29aace1659e14abcfd064b
Саймон <148492517+4IRxx@users.noreply.github.com>
2024-01-28 20:02:24 +0300

    create Lesson_12_junit_5

---
+++ MyHomeworkProjct/src/test/java/FactorialCalculatorTest.java
@@ -0,0 +1,27 @@
+import com.myaqa.FactorialCalculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FactorialCalculatorTest {
+
+    @Test
+    void testFactorialOfZero() {
+        assertEquals(1, FactorialCalculator.calculateFactorial(0));
+    }
+
+    @Test
+    void testFactorialOfOne() {
+        assertEquals(1, FactorialCalculator.calculateFactorial(1));
+    }
+
+    @Test
+    void testFactorialOfPositiveNumber() {
+        assertEquals(120, FactorialCalculator.calculateFactorial(5));
+    }
+
+    @Test
+    void testFactorialOfNegativeNumber() {
+        assertThrows(IllegalArgumentException.class, () -> FactorialCalculator.calculateFactorial(-1));
+    }
+}
