e1816001ca409a49621aad2a23c1ad077e45b5e0
Саймон <148492517+4IRxx@users.noreply.github.com>
2024-02-03 01:33:56 +0300

    update Lesson_14

--- MyHomeworkProjct/src/main/java/com/myaqa/Main.java
+++ MyHomeworkProjct/src/main/java/com/myaqa/Main.java
@@ -1,10 +1,5 @@
 package com.myaqa;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Main {
     public static void main(String[] args) {
         // Все тесты в папке test :)
