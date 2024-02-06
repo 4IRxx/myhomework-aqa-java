9a26431600f8e4d5dac2de124975016599c53893
Саймон <148492517+4IRxx@users.noreply.github.com>
2024-02-06 12:23:31 +0300

    update Lesson_12_junit_5

--- MyHomeworkProjct/src/test/java/FactorialCalculatorTest.java
+++ MyHomeworkProjct/src/test/java/FactorialCalculatorTest.java
@@ -1,26 +1,25 @@
 import com.myaqa.FactorialCalculator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FactorialCalculatorTest {
 
-    @Test
-    void testFactorialOfZero() {
-        assertEquals(1, FactorialCalculator.calculateFactorial(0));
+    @ParameterizedTest(name = "Factorial of {0} should be {1}")
+    @CsvSource({
+            "0, 1",
+            "1, 1",
+            "5, 120"
+    })
+    void testFactorial(int input, int expected) {
+        assertEquals(expected, FactorialCalculator.calculateFactorial(input));
     }
 
     @Test
-    void testFactorialOfOne() {
-        assertEquals(1, FactorialCalculator.calculateFactorial(1));
-    }
-
-    @Test
-    void testFactorialOfPositiveNumber() {
-        assertEquals(120, FactorialCalculator.calculateFactorial(5));
-    }
-
-    @Test
+    @DisplayName("Test factorial calculation with negative number")
     void testFactorialOfNegativeNumber() {
         assertThrows(IllegalArgumentException.class, () -> FactorialCalculator.calculateFactorial(-1));
     }
