ff484ac33610844036fc5452a24df68898286899
Саймон <148492517+4IRxx@users.noreply.github.com>
2024-02-06 11:56:44 +0300

    update Lesson_12_testing

--- MyHomeworkProjct/src/test/java/FactorialCalculatorTestNGTest.java
+++ MyHomeworkProjct/src/test/java/FactorialCalculatorTestNGTest.java
@@ -1,23 +1,24 @@
 import com.myaqa.FactorialCalculator;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
 public class FactorialCalculatorTestNGTest {
-    @Test
-    public void testFactorialOfZero() {
-        Assert.assertEquals(FactorialCalculator.calculateFactorial(0), 1);
+    @DataProvider(name = "factorialTestData")
+    public Object[][] getData() {
+        return new Object[][] {
+                {0, 1},
+                {1, 1},
+                {5, 120}
+        };
     }
 
-    @Test
-    public void testFactorialOfOne() {
-        Assert.assertEquals(FactorialCalculator.calculateFactorial(1), 1);
+    @Test(dataProvider = "factorialTestData", description = "Test factorial calculation with positive numbers")
+    public void testFactorialOfPositiveNumber(int input, int expected) {
+        Assert.assertEquals(FactorialCalculator.calculateFactorial(input), expected, "Factorial calculation is incorrect");
     }
 
-    @Test
-    public void testFactorialOfPositiveNumber() {
-        Assert.assertEquals(FactorialCalculator.calculateFactorial(5), 120);
-    }
-
-    @Test(expectedExceptions = IllegalArgumentException.class)
+    @Test(expectedExceptions = IllegalArgumentException.class, description = "Test factorial calculation with negative number")
     public void testFactorialOfNegativeNumber() {
         FactorialCalculator.calculateFactorial(-1);
     }
