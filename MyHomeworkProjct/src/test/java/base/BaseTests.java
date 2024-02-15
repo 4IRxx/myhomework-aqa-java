f4eb3183c24fe00d8b34771172d954071ded4d67
4IRxx <simanchuk1997@gmail.com>
2024-02-16 01:03:38 +0300

    create Lesson_16

--- MyHomeworkProjct/src/test/java/base/BaseTests.java
+++ MyHomeworkProjct/src/test/java/base/BaseTests.java
@@ -1,5 +1,6 @@
 package base;
 
+import config.ConfProperties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,14 +18,11 @@ public class BaseTests {
 
     @BeforeClass
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:/chromedriver/chromedriver.exe");
-
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
         driver = new ChromeDriver();
-        driver.get("https://www.wildberries.ru/");
+        driver.get(ConfProperties.getProperty("homepage"));
         driver.manage().window().maximize();
-
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-
         homePage = new HomePage(driver, wait);
     }
 
